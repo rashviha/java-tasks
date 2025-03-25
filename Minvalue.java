c943b3cbdb61cf3fcd4591b4539d4f4cbed99f66
Rashviha Murugesan <rashvihamurugesan@gmail.com>
2025-03-25 14:38:00 +0530

    Update Minvalue.java

--- Minvalue.java
+++ Minvalue.java
@@ -1,3 +1,4 @@
+
 class MinValue {
     public static void main(String[] args) {
         // Define the string
@@ -6,7 +7,7 @@ class MinValue {
         // Split the string by commas 
         String[] arr = str.split(",");
         
-        // Initialize min with the largest possible value (or first element)
+        // Initialize min with the largest possible value
         int min = Integer.MAX_VALUE;
         // find the minimum value
         for (int i = 0; i < arr.length; i++) {
@@ -15,8 +16,7 @@ class MinValue {
                 min = num; // Update min if the current number is smaller
             }
         }
-        
-        //To Print the result
+       
         System.out.println("The minimum value is: " + min);
     }
 }
