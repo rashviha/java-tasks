8790514d36b407ad5853a652f07b9174b561adc5
Rashviha Murugesan <rashvihamurugesan@gmail.com>
2025-03-25 14:32:36 +0530

    Update integerposition.java

--- integerposition.java
+++ integerposition.java
@@ -2,21 +2,13 @@ import java.util.Scanner;
 
 class integerposition {
     public static void main(String[] args) {
-        // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
-        
-        // Ask the user to input a string
         System.out.print("Enter a string: ");
-        String inputString = scanner.nextLine(); // Read the entire line
-        
-        // Ask the user to input the number to find
+        String inputString = scanner.nextLine(); 
         System.out.print("Enter a number to find its position: ");
-        String numberToFind = scanner.next(); // Read the number as a string
-        
-        // Find the position of the number
+        String numberToFind = scanner.next(); 
         int position = inputString.indexOf(numberToFind);
         
-        // Check if the number is found and output the result
         if (position != -1) {
             System.out.println("The number '" + numberToFind + "' is found at index: " + position);
         } else {
