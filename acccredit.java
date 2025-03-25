57b898eb617b9b0d0fa86c7fe0ec8ec77288eb1f
Rashviha Murugesan <rashvihamurugesan@gmail.com>
2025-03-25 14:39:08 +0530

    Update acccredit.java

--- acccredit.java
+++ acccredit.java
@@ -6,16 +6,13 @@ class Main {
         Account ac = new Account("Rashviha", 2000000000);
         System.out.println("Account holder name: " + ac.getAccholder());// To Print the current account holder's name and balance
         System.out.println("Balance: " + ac.getBalance());
-        
         // Update the account holder's name
         ac.setAccholder("Dhilip");
         System.out.println("Updated account holder name: " + ac.getAccholder());
         
         System.out.print("Enter amount to credit: ");
         int creditAmount = scanner.nextInt(); 
-        
         ac.addCredit(creditAmount);
-        
         // Print the updated balance
         System.out.println("Updated balance: " + ac.getBalance());
         
@@ -26,22 +23,18 @@ class Account {
     private String Accholder;
     private int Balance;
 
-   
     Account(String Accholder, int Balance) {
         this.Accholder = Accholder;
         this.Balance = Balance;
     }
 
-
     public String getAccholder() {
         return Accholder;
     }
 
-  
     public int getBalance() {
         return Balance;
     }
-
 
     public void setAccholder(String Accholder) {
         this.Accholder = Accholder;
